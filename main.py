9eec2c4602c13c7ef674bfb066d00976842aba87
PatrickHJR <harrisp@ufl.edu>
2022-09-13 19:24:08 -0400

    I added nuggies

--- main.py
+++ main.py
@@ -31,3 +31,7 @@ while option != 0:
     if option == 0:
         myOrder.print_order()
         break
+    if option == 1:
+        myOrder.add_lemonade()
+    if option == 8:
+        myOrder.add_nuggies()
--- order.py
+++ order.py
@@ -9,4 +9,14 @@ class Order:
         print('Here are your items: ', end = '')
         print(*self.items, sep = ', ')
 
+    def add_lemonade(self):
+        self.total_cost += 7.5
+        self.items += ["lemonade"]
+        print("Added Lemonade")
+    
+    def add_nuggies(self):
+        self.total_cost += 13
+        self.items += ["Chicken Nuggies"]
+        print("Added Chicken Nuggies")
+
     # implement methods for menu items
