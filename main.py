fd818a83f912966eb83240e42e928a719bb38d0d
TrevorRichardson0480 <60578933+TrevorRichardson0480@users.noreply.github.com>
2022-09-13 15:49:30 -0400

    workshop ready

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ myOrder = Order()
 
 while option != 0:
     print('')
-    print('1. Drink')
-    print('2. Fries')
-    print('3. Salad')
-    print('4. Shake')
-    print('5. Chicken Nuggies')
-    print('6. Chicken Sandwich')
+    print('1. Lemonade')
+    print('2. Coke')
+    print('3. Cold Brew Coffee')
+    print('4. Fries')
+    print('5. Salad')
+    print('6. Wrap')
+    print('7. Chicken Strips')
+    print('8. Chicken Nuggies')
+    print('9. Kid\'s Meal')
+    print('10. Mac n\' Cheese')
+    print('11. Chicken Biscuit')
+    print('12. Fruit Cup')
+    print('13. Shake')
+    print('14. Frosted Lemonade')
+    print('15. Cookie')
+    print('16. Chicken Sandwich')
     print('0. Done')
     print('Select an option: ')
 
@@ -21,5 +31,3 @@ while option != 0:
     if option == 0:
         myOrder.print_order()
         break
-    if option == 1:
-        myOrder.add_chicken_sandwich()
