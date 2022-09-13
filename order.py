32ad7b64f9debe5fc087dd93d7b28ecb6ac25b50
TrevorRichardson0480 <60578933+TrevorRichardson0480@users.noreply.github.com>
2022-09-13 15:51:07 -0400

    Update order.py

--- order.py
+++ order.py
@@ -9,8 +9,4 @@ class Order:
         print('Here are your items: ', end = '')
         print(*self.items, sep = ', ')
 
-    def add_chicken_sandwich(self):
-        self.total_cost+=5
-        self.items.append("checken sandwich")
-        print("Chicken burger added to order! :)))")
     # implement methods for menu items
